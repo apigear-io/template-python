65c658b650efb407a67996119f7935b0079397fe
dphan10 <dorota.phan@siili.com>
2024-06-03 14:38:20 +0200

    feat: implement imports

--- goldenmaster/tb_empty/api/__init__.py
+++ goldenmaster/tb_empty/api/__init__.py
@@ -1,4 +1,8 @@
 
 from .api import EmptyEnum as EmptyEnum
+from .api import as_empty_enum as as_empty_enum
+from .api import from_empty_enum as from_empty_enum
 from .api import EmptyStruct as EmptyStruct
+from .api import as_empty_struct as as_empty_struct
+from .api import from_empty_struct as from_empty_struct
 from .api import IEmptyInterface as IEmptyInterface
--- goldenmaster/tb_same2/api/__init__.py
+++ goldenmaster/tb_same2/api/__init__.py
@@ -1,8 +1,16 @@
 
 from .api import Enum1 as Enum1
+from .api import as_enum1 as as_enum1
+from .api import from_enum1 as from_enum1
 from .api import Enum2 as Enum2
+from .api import as_enum2 as as_enum2
+from .api import from_enum2 as from_enum2
 from .api import Struct1 as Struct1
+from .api import as_struct1 as as_struct1
+from .api import from_struct1 as from_struct1
 from .api import Struct2 as Struct2
+from .api import as_struct2 as as_struct2
+from .api import from_struct2 as from_struct2
 from .api import ISameStruct1Interface as ISameStruct1Interface
 from .api import ISameStruct2Interface as ISameStruct2Interface
 from .api import ISameEnum1Interface as ISameEnum1Interface
