ef75a86b8c87a4b803c52643658559835903b619
dphan10 <dorota.phan@siili.com>
2024-06-11 14:54:49 +0200

    feat: goldenmaster uses externs and imports

---
+++ goldenmaster/custom_types/olink/sinks.py
@@ -0,0 +1,8 @@
+import asyncio
+from typing import Any
+from olink.core import Name
+from olink.client import IObjectSink, ClientNode
+from utils.eventhook import EventHook
+import utils.base_types
+import custom_types.api
+import logging
---
+++ goldenmaster/extern_types/http/client.py
@@ -0,0 +1,8 @@
+import requests
+import os
+
+from extern_types.api import api
+from . import shared
+import vector3d.vector
+
+
