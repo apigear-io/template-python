ba29dcbeabd3bcafad2469dba5bc1ee6025f5407
dphan10 <dorota.phan@siili.com>
2024-07-01 10:03:49 +0200

    chores(mqtt): clean up mqtt base client
    change order of functions and aling their names with underscores to
    indicate public, private or protected according to python standard
    also adds a comment regarding mqtt configuration parameter

--- templates/apigear/mqtt/client.py
+++ templates/apigear/mqtt/client.py
@@ -14,12 +14,12 @@ logging.getLogger().setLevel(logging.INFO)
 class Client:
     def __init__(self):
         self.send_queue = Queue()
-        self.client = paho.mqtt.client.Client(protocol=paho.mqtt.client.MQTTv5 )
-        self.client.on_connect = self.on_connect
-        self.client.on_subscribe = self.on_subscribed
-        self.client.on_message = self.message_handling
-        self._on_connected = EventHook()
-        self._on_subscribed = EventHook()
+        self.client = paho.mqtt.client.Client(protocol=paho.mqtt.client.MQTTv5)
+        self.client.on_connect = self.__on_connect
+        self.client.on_subscribe = self.__on_subscribed
+        self.client.on_message = self.__message_handling
+        self.on_connected = EventHook()
+        self.on_subscribed = EventHook()
         self.topics = {}
         self.on_log(self._mqtt_log_writer)
         
@@ -33,27 +33,18 @@ class Client:
         self.client.loop_stop()
         self.client.disconnect()
      
-    def on_subscribed(self, client, userdata, mid, reason_code_list, properties):
-        self._on_subscribed.fire()
-        
-    #TODO this should be probable setRemoteProperty etc.
-    def publish(self, topic, payload, qos):
-        self.client.publish(topic, payload, qos)
-     
     async def connect(self, addr, port):
         self.client.loop_start()
         bind_address=""
-        clean_start=True
+        #TODO - should it be clean start?
+        # True: all messages, published by other publishers will be retained in broker untill this  subscriber explicitly unsubscribed
+        ## all the messages to publish from this client will be stored anyway and sent on reconnect ( if this is not expected reinitialise needs to be called along with re-subscription)
+        clean_start=False
         properties = paho.mqtt.properties.Properties(paho.mqtt.properties.PacketTypes.CONNECT)
         keepalive = 60
         self.client.connect_async(addr, port, keepalive, bind_address, clean_start, properties)
-        
-    def on_connect(self, client, userdata, flags, reasonCode, properties=None):
-        #TODO subscribe all waiting to subscribe
-        self._on_connected.fire()
-            
+
     def subscribe(self, topic, callback):
-        self.topics
         if topic not in self.topics:
             self.topics[topic] = callback
             self.client.subscribe(topic, 2)
@@ -66,14 +57,27 @@ class Client:
             self.client.unsubscribe(topic)
         else:
             self.logging_func(paho.mqtt.enums.LogLevel.MQTT_LOG_WARNING, "topic not there, not removed")
+       
+    def publish(self, topic, payload, qos):
+        self.client.publish(topic, payload, qos)
 
-    def message_handling(self, client, userdata, msg):
+    def on_log(self, logging_func):
+        self.logging_func = logging_func
+        self.client.on_log = self.__on_log
+        
+    def __on_connect(self, client, userdata, flags, reasonCode, properties=None):
+        #TODO subscribe all waiting to subscribe
+        self.on_connected.fire()
+            
+    def __message_handling(self, client, userdata, msg):
         callback = self.topics.get(msg.topic)
         if callback != None:
             callback(msg.payload.decode())    
         else:
             self.logging_func(paho.mqtt.enums.LogLevel.MQTT_LOG_WARNING, f"not handled: {msg.topic}: {msg.payload.decode()}")
-        
+
+    def __on_subscribed(self, client, userdata, mid, reason_code_list, properties):
+        self.on_subscribed.fire()        
         
     def _mqtt_log_writer(self, level, msg):
        if level == paho.mqtt.enums.LogLevel.MQTT_LOG_DEBUG:
@@ -87,10 +91,6 @@ class Client:
        else:
            logging.error(msg)
 
-    def on_log(self, logging_func):
-        self.logging_func = logging_func
-        self.client.on_log = self._on_log
-
-    def _on_log(self, client, userdata, paho_log_level, messages):
+    def __on_log(self, client, userdata, paho_log_level, messages):
         if self.logging_func != None:
             self.logging_func(paho_log_level, messages)
