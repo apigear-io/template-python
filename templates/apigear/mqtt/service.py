d71a111a2c8ea5da490e4d9d35d7ebcb4d2064c3
dphan10 <dorota.phan@siili.com>
2024-07-04 10:59:04 +0200

    chores(mqtt): differentiate server subscriptions
    depending on action type the subscribed callback may have different
    parameters

--- templates/apigear/mqtt/service.py
+++ templates/apigear/mqtt/service.py
@@ -1,16 +1,23 @@
 from .base import BaseClient
+import paho.mqtt.properties
+import paho.mqtt.client
 
 class Service(BaseClient):
     def __init__(self, id):
         super().__init__(id)
         
-    def subscribe_for_invoke_req(topic, callback):
-        #TODO 
-        # handling invoke resp messages should be different than handling singals or properties messages
-        # the user callback should wrapped with a function that extracts the responseTopic and correlation Info
-        # the method should execute user callback in async manner, the callback should return data to be sent
-        # the callback wrapper should send the notifyInvokeReply 
-        print(topic)
+    def subscribe(self, topic, callback):
+        self._subscribe(topic, callback, self.pass_only_payload)
+    
+    def invoke_handler_wrapper(self,msg : paho.mqtt.client.MQTTMessage, callback ):
+        payload = msg.payload.decode()
+        result = callback(payload)
+        properties = paho.mqtt.properties.Properties(paho.mqtt.properties.PacketTypes.PUBLISH)
+        properties.CorrelationData = msg.properties.CorrelationData
+        self.notify_invoke_response(msg.properties.ResponseTopic, result, properties)
+              
+    def subscribe_for_invoke_req(self, topic, callback):
+        self._subscribe(topic, callback, self.invoke_handler_wrapper)
         
     def notify_invoke_response(self, responseTopic, payload, propsWithCorrelationData):
         self.client.publish(responseTopic, payload, self.qos, retain = False, properties = propsWithCorrelationData)
