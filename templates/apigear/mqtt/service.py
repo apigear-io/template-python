5f7845969e22b3e4f26456794466225bb67bd876
dphan10 <dorota.phan@siili.com>
2024-07-04 10:59:04 +0200

    feat(mqtt): handle invoke requests in server
    adds subscribing for invoke requests which needs to send response to a
    client. It adds a wrapper which handles this and just calls user
    callback.
    The wrapper uses message properties to give proper response to a client.

--- templates/apigear/mqtt/service.py
+++ templates/apigear/mqtt/service.py
@@ -1,5 +1,7 @@
 from .base import BaseClient
 from typing import Callable, Any
+import paho.mqtt.properties
+import paho.mqtt.client
 
 class Service(BaseClient):
     def __init__(self, id):
@@ -7,14 +9,20 @@ class Service(BaseClient):
         
     def subscribe_for_property(self, topic, callback: Callable[[Any], None]):
         self._subscribe(topic, callback, self.pass_only_payload)
-        
-    def subscribe_for_invoke_req(topic, callback):
-        #TODO 
-        # handling invoke resp messages should be different than handling signals or properties messages
-        # the user callback should wrapped with a function that extracts the responseTopic and correlation Info
-        # the method should execute user callback in async manner, the callback should return data to be sent
-        # the callback wrapper should send the notifyInvokeReply 
-        print("handling invoke request is not yet handled")
+    
+    def invoke_handler_wrapper(self, msg : paho.mqtt.client.MQTTMessage, callback):
+        payload = self.from_payload(msg.payload)
+        result = ""
+        if callback != None:
+            result = callback(payload)
+        else:
+            self.logging_func(paho.mqtt.enums.LogLevel.MQTT_LOG_WARNING, f"no callback for: {msg.topic}: {msg.payload.decode()}")
+        properties = paho.mqtt.properties.Properties(paho.mqtt.properties.PacketTypes.PUBLISH)
+        properties.CorrelationData = msg.properties.CorrelationData
+        self.notify_invoke_response(msg.properties.ResponseTopic, result, properties)
+              
+    def subscribe_for_invoke_req(self, topic, callback: Callable[[list[Any]], None]):
+        self._subscribe(topic, callback, self.invoke_handler_wrapper)
         
     def notify_invoke_response(self, responseTopic, payload, propsWithCorrelationData):
         self.client.publish(responseTopic, self.to_payload(payload), self.qos, retain = False, properties = propsWithCorrelationData)
