a30e52df0b6982b9e08129f57506c27e470fe85c
dphan10 <dorota.phan@siili.com>
2024-07-05 14:38:02 +0200

    feat(mqtt): handle invoke responses in client
    client now gives an id to each outgoing invoke request
    the id should be unique among all subscribers for same operation type
    (topic) the subscriber should store the id and accept only those
    responses with matching id (among the topic, but in this implementations
    we have one generator for all operations).
    The python integers do not overflow.

--- templates/apigear/mqtt/client.py
+++ templates/apigear/mqtt/client.py
@@ -1,32 +1,50 @@
 from .base import BaseClient
 import paho.mqtt.properties
 from typing import Callable, Any 
-
+import multiprocessing
 
 class Client(BaseClient):
     def __init__(self, id):
         super().__init__(id)
+        self.id_generator = self.IdGenerator()
         
     def subscribe_for_property(self, topic, callback: Callable[[Any], None]):
         self._subscribe(topic, callback, self.pass_only_payload)
 
     def subscribe_for_signal(self, topic, callback: Callable[[list[Any]], None]):
         self._subscribe(topic, callback, self.pass_only_payload)
+        
+    def invoke_resp_handler_wrapper(self,msg : paho.mqtt.client.MQTTMessage, callback ):
+        payload = self.from_payload(msg.payload)
+        correlationData = int.from_bytes(msg.properties.CorrelationData,"big")
+        if callback != None:
+            callback(payload, correlationData)
+        else:
+            self.logging_func(paho.mqtt.enums.LogLevel.MQTT_LOG_WARNING, f"no callback for: {msg.topic}: {msg.payload.decode()}")
+              
+    def subscribe_for_invoke_resp(self, topic, callback: Callable[[Any, int],None]):
+        self._subscribe(topic, callback, self.invoke_resp_handler_wrapper)
 
     def set_remote_property(self, topic, payload_value):
         self.client.publish(topic, self.to_payload(payload_value), self.qos, retain = False)
-        
-    def subscribe_for_invoke_resp(topic, invokeCallback):
-        #TODO 
-        # handling invoke resp messages should be different than handling signals or properties messages
-        # the user callback should get one more parameter(respId) and it should be wrapped with a function
-        # that parses properties to extract the id and then calls user callback (topic, payload, respId)
-        print(topic)
- 
-    
-    def invoke_remote(self, topic, responseTopic, payload, responseId):    
+   
+    def invoke_remote(self, topic, responseTopic, payload):    
+        responseId = self.id_generator.get_unique_id()
         props = paho.mqtt.properties.Properties(paho.mqtt.properties.PacketTypes.PUBLISH)
-        props.responseTopic=responseTopic
-        props.CorrelationData = bytes(responseId)
-        self.client.publish(responseTopic,  self.to_payload(payload), self.qos, retain = False, properties = props)
-        print(topic)
+        props.ResponseTopic = responseTopic
+        props.CorrelationData = bytes([responseId])
+        self.client.publish(topic, self.to_payload(payload), self.qos, retain = False, properties = props)
+        return responseId
+    
+    class IdGenerator:
+        def __init__(self):
+            self.id = 0
+            self.lock_manager = multiprocessing.Manager()
+            self.lock = self.lock_manager.Lock()
+
+        def get_unique_id(self):
+            self.lock.acquire()
+            unique_id = self.id
+            self.id += 1
+            self.lock.release() 
+            return unique_id
